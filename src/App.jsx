678f38e42364b9b1edd23a1caa14eb70cc7875d2
Anshul <anshul4144@gmail.com>
2026-04-02 16:35:41 +0530

    removed portfolio section

--- src/App.jsx
+++ src/App.jsx
@@ -5,7 +5,7 @@ import Home from "./pages/Home/Home";
 import Services from "./pages/Services/Services";
 import SmartHelp from "./pages/SmartHelp/SmartHelp";
 import About from "./pages/About/About";
-import Portfolio from "./pages/Portfolio/Portfolio";
+
 import Booking from "./pages/Booking/Booking";
 function App() {
   return (
@@ -17,7 +17,7 @@ function App() {
         <Route path="/services" element={<Services />} />
         <Route path="/smart-help" element={<SmartHelp />} />
         <Route path="/about" element={<About />} />
-        <Route path="/portfolio" element={<Portfolio/>} />
+        
         <Route path="/booking" element={<Booking />} />
       </Routes>
       <Footer/>
